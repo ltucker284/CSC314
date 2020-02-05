390dfabc4bc1657dace7a8a0cc7cded59de702e4
Liz Tucker <ltucker284@gmail.com>
2020-02-05 16:19:34 -0500

    2b

---
+++ lab2.py
@@ -0,0 +1,5 @@
+sequence = input('Input your sequence here: ')
+sequence = sequence.upper()
+sequence_length = len(sequence)
+print('The sequence you entered is: 5\' - '+ sequence +' - 3\'. \n The length of the sequence is: ' + str(sequence_length))
+
