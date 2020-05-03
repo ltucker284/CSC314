8396c33ab96e0cfac7b4c8160ebff2ecde939a4f
Liz Tucker <ltucker284@gmail.com>
2020-05-02 22:32:40 -0400

    working on calc global alignment

--- final-project/main.py
+++ final-project/main.py
@@ -11,7 +11,7 @@ from pprint import pprint
 def chart_maker(seq_1, seq_2, len_seq_1,len_seq_2):
     index=0
     #create chart
-    chart=[['' for row in range(len_seq_1 + 2)] 
+    chart=[[0 for row in range(len_seq_1 + 2)] 
             for col in range(len_seq_2 + 2)] 
     ######POPULATE SEQUENCES###########
     #populate first horizontal row with sequence 1
@@ -24,18 +24,23 @@ def chart_maker(seq_1, seq_2, len_seq_1,len_seq_2):
 
 """Calculates the dynamic programming chart"""
 def calc_global_alignment(seq_1, seq_2, chart):
+    index=0
     gap_penalty = -4
     match = 5
     mismatch = -1
-    score = 0
-
-
+    c_index=0
+    for lst in chart[2:]:
+        for item in lst[2:]:
+            if chart[0][c_index] == chart[c_index][0]:
+                print(chart[c_index][c_index])
+        c_index+=1
+    return chart
 
 def main():
     seq_1 = 'taca'
     seq_2 = 'ttcag'
     chart = chart_maker(seq_1, seq_2, len(seq_1), len(seq_2))
-    calc_global_alignment(seq_1, seq_2, chart)
+    chart = calc_global_alignment(seq_1, seq_2, chart)
     pprint(chart)
 
 if __name__ == "__main__":
