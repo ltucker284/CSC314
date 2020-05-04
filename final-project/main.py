697427d004634f5e4fe501f80a7254cdca77e041
Liz Tucker <ltucker284@gmail.com>
2020-05-04 13:05:51 -0400

    finished cal_global_alignment

--- final-project/main.py
+++ final-project/main.py
@@ -17,30 +17,52 @@ def chart_maker(seq_1, seq_2, len_seq_1,len_seq_2):
     #populate first horizontal row with sequence 1
     chart[0][2:]=[letter for letter in seq_1]
     #populate first vertical col with second sequence and second vertical col with calculations
-    chart[2:]=[[seq_2[index], (-4*index)] + lst[2:] for index,lst in enumerate(chart[2:]) for item in lst[:-len(lst)+1]]
+    chart[2:]=[[seq_2[index], (-4*(index+1))] + lst[2:] for index,lst in enumerate(chart[2:]) for item in lst[:-len(lst)+1]]
     #populate first horizontal line with calculations
     chart[1][1:]=[(-4*index) for index, entry in enumerate(chart[1][1:])]
     return chart
 
 """Calculates the dynamic programming chart"""
-def calc_global_alignment(seq_1, seq_2, chart):
-    index=0
+def calc_global_alignment(chart):
+    index = 1 #vertical index, starting at one to be able to pass the correct value into eval_highest
+    v_index=2 #vertical index used to store answers in the chart
+    for lst in chart[2:]:
+        h_index=2 #horizontal index
+        for item in lst[2:]:
+            chart[v_index][h_index] = eval_highest(chart[index][h_index-1], chart[v_index][h_index-1], chart[index][h_index], determine_match(chart[v_index][0],chart[0][h_index]))
+            h_index+=1
+        v_index+=1
+        index+=1
+    return chart
+
+"""Returns true if two character match, false otherwise"""
+def determine_match(char_a, char_b):
+    if char_a == char_b: return True
+    else: return False
+
+"""Evaluates the highest score for a given spot"""
+def eval_highest(p_char, h_gap, v_gap, b_match):
     gap_penalty = -4
     match = 5
     mismatch = -1
-    c_index=0
-    for lst in chart[2:]:
-        for item in lst[2:]:
-            if chart[0][c_index] == chart[c_index][0]:
-                print(chart[c_index][c_index])
-        c_index+=1
-    return chart
+    calc = []
+    if b_match == True:
+        temp = p_char + match
+        calc.append(temp)
+    else:
+        temp = p_char + mismatch
+        calc.append(temp)
+    cal_1 = h_gap + gap_penalty
+    cal_2 = v_gap + gap_penalty
+    calc.append(cal_1)
+    calc.append(cal_2)
+    return max(calc)
 
 def main():
     seq_1 = 'taca'
     seq_2 = 'ttcag'
     chart = chart_maker(seq_1, seq_2, len(seq_1), len(seq_2))
-    chart = calc_global_alignment(seq_1, seq_2, chart)
+    chart = calc_global_alignment(chart)
     pprint(chart)
 
 if __name__ == "__main__":
